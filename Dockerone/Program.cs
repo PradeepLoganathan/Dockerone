8ac6faa73876a0d7226190375f80c9828c73602f
Pradeep Loganathan <pradeep@pradeeploganathan.com>
2018-07-20 18:58:57 +0530

    Initial commit

---
+++ Dockerone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dockerone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");            
+        }
+    }
+}
